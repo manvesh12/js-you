f857ac381da7ce9f7fcc19665d0c31d7a2c1a20d
manvesh12 <168457140+manvesh12@users.noreply.github.com>
2025-01-06 17:07:47 +0000

    datatypes and variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId =  144553
+let accountemail = "singhmanvesh04@gmail.com"
+var accountpassword = "12345"
+accountcity = "jaipur"
+accountstate
+//accountId = 2
+// not allowed to change teh value of constant value 
+console.log(accountId);
+
+//tabular structure 
+console.table([accountId,accountemail,accountpassword,accountcity,accountstate])
+// prefer not to use var : because of issue in functional and block scope 
