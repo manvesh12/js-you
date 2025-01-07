ee6cdd90e2749c23ddc0fe38e62e2468ca99a99a
manvesh12 <168457140+manvesh12@users.noreply.github.com>
2025-01-07 13:09:34 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,16 @@
+let score = "33"
+console.log(typeof score );
+console.log(typeof (score ));
+
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//for conversoin we store the value of variable after conversion in another valraible 
+//and then get the type of the new variable 
+
+//can convert into boolean
+//output can be NaN if the string is given and we convet it into a number 
+//if we convert empty string then we get the answer as false 
+// if we convert any string to boolean then answer is true
